ec70bb9156f7746a3fd49f0f0353c582c5ec4c37
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-07 17:28:57 -0300

    feat(helpers): Cria funções para gerarem e verificarem jwt tokens

---
+++ src/helpers/JwtToken.ts
@@ -0,0 +1,20 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+const SECRET = process.env.JWT_SECRET || 'secret';
+
+const jwtConfig: object = {
+  algorithm: 'H256',
+};
+
+const generateToken = (payload: object): string => {
+  const token: string = jwt.sign(payload, SECRET, jwtConfig);
+  return token;
+};
+
+const verifyToken = (token: string): string | JwtPayload => (
+  jwt.verify(token, SECRET, jwtConfig));
+
+export default {
+  generateToken,
+  verifyToken,
+};
