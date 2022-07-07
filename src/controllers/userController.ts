16bc41d1d2fb0786c4de30bcc75d51524e9066f3
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-07 17:12:42 -0300

    feat(routes): Adiciona rota post "/users"

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import userService from '../services/userService';
 
 const create = async (req: Request, res: Response) => {
-  const result = await userService.create(user);
+  const result = await userService.create(req.body);
   res.status(201).json(result);
 };
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import productsRoute from './productsRoute';
+import usersRoute from './usersRoute';
 
 const router = Router();
   
 router.use('/products', productsRoute);
+router.use('/users', usersRoute);
 
 export default router;
