93cd3dd6f8ac56fc08f7f2788faf00cf0d28bc81
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-07 15:33:06 -0300

    feat(controllers): Cria função getAll no productsController

---
+++ src/controllers/productsController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import Iproducts from '../interfaces/productsInterface';
+import productsService from '../services/productsService';
+
+const getAll = async (req: Request, res: Response): Promise<Response> => {
+  const products: Iproducts[] = await productsService.getAll();
+  return res.status(200).json(products);
+};
+
+export default {
+  getAll,
+};
