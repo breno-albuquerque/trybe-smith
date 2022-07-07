38ea7819c323c2d55a3d1a607d7d1bc5f4d6df80
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-07 17:20:35 -0300

    feat(routes): Adiciona os middlewares de validação na rota

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,8 +1,22 @@
 import { Router } from 'express';
 import userController from '../controllers/userController';
 
+import { 
+  validateUserName,
+  validateUserClass,
+  validateUserLevel,
+  validateUserPassword,
+} from '../middlewares/validateUser';
+
 const router = Router();
 
-router.post('/users', userController.create);
+router.post(
+  '/users',
+  validateUserName,
+  validateUserClass,
+  validateUserLevel,
+  validateUserPassword,
+  userController.create,
+);
 
 export default router;
