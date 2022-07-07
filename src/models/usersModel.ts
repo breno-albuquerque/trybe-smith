749e2774b7b0ed2edc857916310ebe4bb1391dc6
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-07 17:04:56 -0300

    feat(models): Adiciona função create em usersModel

---
+++ src/models/usersModel.ts
@@ -0,0 +1,18 @@
+import { ResultSetHeader } from 'mysql2';
+import connection from './connection';
+import Iuser from '../interfaces/userInterface';
+
+const create = async (user: Iuser): Promise<number | null> => {
+  const { username, classe, level, password } = user;
+  const query = `
+  INSERT INTO Trybesmith.Users (username, classe, level, passwod) VALUE (?, ?, ?, ?)
+  `;
+  const [result] = await connection
+    .execute<ResultSetHeader>(query, [username, classe, level, password]);
+  const { insertId } = result;
+  return insertId;
+};
+
+export default {
+  create,
+};
