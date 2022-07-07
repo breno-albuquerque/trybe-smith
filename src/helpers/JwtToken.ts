1d131624499fdd66891153651d8e9bd70043eaa8
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-07 20:44:15 -0300

    refactor(helpers): Refatora tipagem e imports em JwtTokens

--- src/helpers/JwtToken.ts
+++ src/helpers/JwtToken.ts
@@ -1,18 +1,18 @@
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import { JwtPayload, sign, SignOptions, verify } from 'jsonwebtoken';
 
 const SECRET = process.env.JWT_SECRET || 'secret';
 
-const jwtConfig: object = {
+const jwtConfig: SignOptions = {
   algorithm: 'HS256',
 };
 
 const generateToken = (payload: object): string => {
-  const token: string = jwt.sign(payload, SECRET, jwtConfig);
+  const token: string = sign(payload, SECRET, jwtConfig);
   return token;
 };
 
 const verifyToken = (token: string): string | JwtPayload => (
-  jwt.verify(token, SECRET, jwtConfig));
+  verify(token, SECRET, jwtConfig));
 
 export default {
   generateToken,
