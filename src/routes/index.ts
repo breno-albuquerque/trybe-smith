eac0cbc28517b730a13986c23abe3144e0b71103
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-08 08:28:47 -0300

    feat(service): Adiciona rota post("/login")

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import productsRoute from './productsRoute';
 import usersRoute from './usersRoute';
 import ordersRoute from './ordersRoute';
+import loginRoute from './loginRoute';
 
 const router = Router();
   
 router.use('/products', productsRoute);
 router.use('/users', usersRoute);
 router.use('/orders', ordersRoute);
+router.use('/login', loginRoute);
 
 export default router;
