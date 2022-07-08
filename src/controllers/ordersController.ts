fdae4037b27b6a89613947daf951975fa57e0df5
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-07 21:00:33 -0300

    feat(controllers): Cria função getAll em ordersController

---
+++ src/controllers/ordersController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import ordersService from '../services/ordersService';
+
+const getAll = async (_req: Request, res: Response) => {
+  const fullOrder = await ordersService.getAll();
+  return res.status(200).json(fullOrder);
+};
+
+export default {
+  getAll,
+};
