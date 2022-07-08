1de26fa76c6c1cabbd67bcc1aa528a6c40900c66
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-08 08:38:05 -0300

    fix(middlewares): Muda desestruturação de name para username

--- src/middlewares/validateLogin.ts
+++ src/middlewares/validateLogin.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 
 const validateLogin = (req: Request, res: Response, next: NextFunction): Response | void => {
-  const { name, password } = req.body;
-  if (!name) return res.status(400).json({ message: '"username" is required' });
+  const { username, password } = req.body;
+  if (!username) return res.status(400).json({ message: '"username" is required' });
   if (!password) return res.status(400).json({ message: '"password" is required' });
   next();
 };
