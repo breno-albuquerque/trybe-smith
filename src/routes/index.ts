d298bee2f054a5a103df302badd09e5725854cad
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-07 21:03:33 -0300

    feat(routes): Cria rota get("/orders")

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import productsRoute from './productsRoute';
 import usersRoute from './usersRoute';
+import ordersRoute from './ordersRoute';
 
 const router = Router();
   
 router.use('/products', productsRoute);
 router.use('/users', usersRoute);
+router.use('/orders', ordersRoute);
 
 export default router;
