01fa0bfbcafbe0ffe28afcdebd77056e3488fe86
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-09 20:28:03 -0300

    refactor(products): Refatora caminho completo da rota products

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,15 +1,13 @@
 import { Request, Response } from 'express';
-import IProduct from '../interfaces/productsInterface';
 import productsService from '../services/productsService';
 
 const getAll = async (_req: Request, res: Response): Promise<Response> => {
-  const products: IProduct[] = await productsService.getAll();
+  const products = await productsService.getAll();
   return res.status(200).json(products);
 };
 
 const create = async (req: Request, res: Response): Promise<Response> => {
-  const product: IProduct = req.body;
-  const newProduct = await productsService.create(product);
+  const newProduct = await productsService.create(req.body);
   return res.status(201).json(newProduct);
 };
 
--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -2,12 +2,12 @@ import IProduct from '../interfaces/productsInterface';
 import productsModel from '../models/productsModel';
 
 const getAll = async (): Promise<IProduct[]> => {
-  const products: IProduct[] = await productsModel.getAll();
+  const products = await productsModel.getAll();
   return products;
 };
 
 const create = async (product: IProduct): Promise<IProduct> => {
-  const insertId: number = await productsModel.create(product);
+  const insertId = await productsModel.create(product);
   return { id: insertId, ...product };
 };
 
