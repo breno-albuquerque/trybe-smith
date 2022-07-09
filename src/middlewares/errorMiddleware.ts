f1888ce8d02facedd049e46efd72b054c54c5929
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-09 19:26:33 -0300

    feat(middlewares): Cria middleware de erro

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+import HttpError from '../helpers/httpError';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const errorMiddleware = (error: Error, _req: Request, res: Response, _next: NextFunction) => {
+  const { status, message } = error as HttpError;
+  return res.status(status || 500).json(message || { message: 'Internal error' });
+};
+
+export default errorMiddleware;
