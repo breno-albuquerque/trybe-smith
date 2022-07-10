9b9ca52e6af6cbdd6ee1b476a58870a5cebf1433
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-09 23:10:39 -0300

    feat(routes): Rota de produtos agora também possui middleware de autenticação

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import productsController from '../controllers/product.controller';
+import auth from '../middlewares/auth.middleware';
 import { validateProductName, validateProductAmount } from '../middlewares/product.middleware';
 
 const router = Router();
 
-router.get('/', productsController.getAll);
-router.post('/', validateProductName, validateProductAmount, productsController.create);
+router.get('/', auth, productsController.getAll);
+router.post('/', auth, validateProductName, validateProductAmount, productsController.create);
 
 export default router;
